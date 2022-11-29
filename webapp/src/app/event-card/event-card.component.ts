203f67c1b71148028e0ee6fb2a6591a7576b33a5
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2022-11-29 14:15:02 -0500

    event card icons reflects the values

--- webapp/src/app/event-card/event-card.component.ts
+++ webapp/src/app/event-card/event-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { PathConstants } from '../constants';
+import { ParserService } from '../services/swallow-json-parser/parser.service';
 import { SwallowEntry } from '../services/swallow-json-parser/swallow-entry';
 
 @Component({
@@ -12,8 +13,10 @@ export class EventCardComponent implements OnInit {
   recordingAvailable: boolean = false;
   recordingURL!: string;
   @Input() entry!: SwallowEntry | null;
+  duration: string = "";
+  genre: string = "";
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public parser: ParserService) { }
 
   ngOnInit(): void {
     if (this.entry) {
@@ -25,6 +28,8 @@ export class EventCardComponent implements OnInit {
         }
       }
     }
+    this.duration = this.getDuration();
+    this.genre = this.getGenre();
   }
 
   onClickMore(): void {
@@ -33,5 +38,59 @@ export class EventCardComponent implements OnInit {
 
   onOpenVideoURL(): void {
     window.open(this.recordingURL, "_blank");
+  }
+
+  getDuration(): string {
+    if (!this.entry) {
+      return "";
+    }
+    const durations = this.entry.Digital_File_Description.map((fileDesc) => {
+      return fileDesc.duration;
+    }).filter((duration) => {
+      return duration && duration.trim().length > 0
+    });
+    let maxDuration = this.durationToSeconds(durations[0]);
+    for (let i = 1; i < durations.length; i++) {
+      let duration = this.durationToSeconds(durations[i]);
+      if (duration > maxDuration) {
+        maxDuration = duration;
+      }
+    }
+
+    let hours = Math.floor(maxDuration / 3600);
+    let minutes = Math.floor((maxDuration % 3600) / 60);
+    let seconds = Math.floor(maxDuration % 3600 % 60);
+
+    if (hours > 0) {
+      return hours + (hours > 1 ? " hours +" : " hour +");
+    } else if (minutes > 0) {
+      return minutes + (minutes > 1 ? " minutes +" : " minute +");
+    }
+    return seconds + (seconds > 1 ? " seconds +" : " second +");
+  }
+
+  durationToSeconds(duration: string): number {
+    if (!duration || !duration.trim().length) {
+      return 0;
+    }
+    const durationInt = duration.split(":").map((t) => {
+      try {
+        return Number(t);
+      } catch (e) {
+        return 0;
+      }
+    });
+    let seconds = durationInt[durationInt.length - 1];
+    for (let i = durationInt.length - 2; i >= 0; i--) {
+      seconds += durationInt[i] * Math.pow(60, durationInt.length - 1 - i);
+    }
+    return seconds;
+  }
+
+  getGenre(): string {
+    if (!this.entry) {
+      return "";
+    }
+    return this.parser.getGenres(this.entry.Item_Description);
   }
 }
