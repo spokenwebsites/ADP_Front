3f72ad3e780630c091fe17b2d93f3e15bf3eb0e6
CONCORDIA\tneugeba <tomasz.neugebauer@concordia.ca>
2023-07-25 17:28:01 -0400

    Improvements and fixes to condegram (viz1): loading component added, remove the unnecessary/duplicate link rel CSS style in body of the component, changed global styles.css for alignment of viz1

--- webapp/src/app/viz1/viz1.component.ts
+++ webapp/src/app/viz1/viz1.component.ts
@@ -13,32 +13,44 @@ declare function get_condegram_json(json: any, onCLick: Function): any;
 })
 export class Viz1Component implements OnInit {
 
-  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {
+  isLoading = true;
+
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.isLoading = true;
     this.http.get<any>('assets/js/Spiralcondegram_final.json')
       .subscribe(
         data => {
           const onClick = this.onClick.bind(this);
           get_condegram_json(data, onClick);
+          console.log('LOADED JSON DATA');
+          this.isLoading = false;
         },
         error => {
         }
       );
-  }
 
-  ngOnInit(): void {
     this.loadJsFile("https://cdnjs.cloudflare.com/ajax/libs/d3/7.3.0/d3.min.js");
     this.loadJsFile("https://d3js.org/d3.v4.js")
-    // this.loadJsFile("assets/js/d3.min.js");
-    // this.loadJsFile("assets/js/d3.v4.js")
-
     this.loadJsFile("assets/js/Condegram_main.js");
   }
 
   public loadJsFile(url: string) {
+
+    this.isLoading = true;
+
     let node = document.createElement('script');
     node.src = url;
     node.type = 'text/javascript';
-    document.getElementsByTagName('head')[0].appendChild(node);
+
+    // Add an event listener for the script's load event
+    node.onload = () => {
+      this.isLoading = false;
+      console.log("LOADED: "+url);
+    };
+
+    document.getElementsByTagName('body')[0].appendChild(node);
   }
 
   onClick(dateStr: any): void {
@@ -47,5 +59,5 @@ export class Viz1Component implements OnInit {
     let day = String(date.getDate()).padStart(2, '0');
     let d = `${date.getFullYear()}-${month}-${day}`;
     this.router.navigate([PathConstants.Dashboard], { queryParams: { filter: d, type: FilterType.Date }, relativeTo: this.route });
-  }
+ }
 }
